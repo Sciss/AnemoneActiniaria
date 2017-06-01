5b02720262be3e8e803a01575e7385d859f8c609
Hanns Holger Rutz <contact@sciss.de>
2017-06-01 13:34:43 +0200

    step

--- src/main/scala/de/sciss/anemone/Cracks.scala
+++ src/main/scala/de/sciss/anemone/Cracks.scala
@@ -13,10 +13,13 @@
 
 package de.sciss.anemone
 
+import java.net.{InetAddress, InetSocketAddress, SocketAddress}
+
 import de.sciss.file._
 import de.sciss.lucre.stm.Sys
 import de.sciss.lucre.synth.{Node, Server, Synth, Txn}
 import de.sciss.nuages.{DbFaderWarp, ExponentialWarp, Nuages, ParamSpec, ScissProcs}
+import de.sciss.synth.UGenSource.Vec
 import de.sciss.synth.io.AudioFile
 import de.sciss.synth.proc.AudioCue
 import de.sciss.synth.proc.graph.impl.SendReplyResponder
@@ -24,7 +27,34 @@ import de.sciss.synth.ugen.ControlValues
 import de.sciss.synth.{SynthGraph, addAfter}
 import de.sciss.{nuages, osc}
 
+import scala.util.control.NonFatal
+
 object Cracks {
+  final val DEBUG = false
+
+  val raspiIPs: Vec[String] = Vector(
+    "192.168.0.11",
+    "192.168.0.12",
+    "192.168.0.13",
+    "192.168.0.24"
+  )
+
+  val myIP: String = "192.168.0.77"
+
+  val raspiPort: Int = 57120
+
+  private[this] val raspiSockets: Array[SocketAddress] = raspiIPs.map { ip =>
+    new InetSocketAddress(ip, raspiPort)
+  } (collection.breakOut)
+
+  private[this] val raspiT: osc.UDP.Transmitter.Undirected = {
+    val config = osc.UDP.Config()
+    config.localAddress = InetAddress.getByName(myIP)
+    val t = osc.UDP.Transmitter(config)
+    t.connect()
+    t
+  }
+
   private final class CrackResponder(protected val synth: Node)
     extends SendReplyResponder {
 
@@ -32,13 +62,52 @@ object Cracks {
 
     protected def added()(implicit tx: Txn): Unit = ()
 
+    private[this] val data = new Array[Int](8)
+
+    private[this] val haveWarned = new Array[Boolean](4)
+
     protected def body: PartialFunction[osc.Message, Unit] = {
       case osc.Message("/cracks", NodeID, 0,
           x1: Float, y1: Float, x2: Float, y2: Float, x3: Float, y3: Float, x4: Float, y4: Float) =>
 
-        val x1i = (x1 * width ).toInt
-        val y1i = (y1 * height).toInt
-        println(s"x1 $x1i, y1 $y1i")
+        val d = data
+        d(0) = (x1 * width ).toInt
+        d(1) = (y1 * height).toInt
+
+        d(2) = (x2 * width ).toInt
+        d(3) = (y2 * height).toInt
+
+        d(4) = (x3 * width ).toInt
+        d(5) = (y3 * height).toInt
+
+        d(6) = (x4 * width ).toInt
+        d(7) = (y4 * height).toInt
+
+        if (DEBUG) println(s"CRACK COORD 1: x1 ${d(0)}, y1 ${d(1)}")
+
+        val sock  = raspiSockets
+        val t     = raspiT
+        var i = 0
+        var j = 0
+        while (i < 4) {
+          val target  = sock(i)
+          val xi      = d(j)
+          j += 1
+          val yi      = d(j)
+          j += 1
+          val p = osc.Message("/xy", xi, yi)
+          try {
+            t.send(p, target)
+          } catch {
+            case NonFatal(ex) =>
+              if (!haveWarned(i)) {
+                haveWarned(i) = true
+                println(s"COULD NOT SEND TO RASPI ${i+1}")
+                ex.printStackTrace()
+              }
+          }
+          i += 1
+        }
     }
   }
 
@@ -57,6 +126,7 @@ object Cracks {
 
     tx.afterCommit {
       println("Launched crack router")
+
     }
   }
 
@@ -100,10 +170,10 @@ object Cracks {
 
         val cx    = pAudio("cx", ParamSpec(0.0, 1.0), default(0.5))
         val cy    = pAudio("cy", ParamSpec(0.0, 1.0), default(0.5))
-        val rx0   = pAudio("rx", ParamSpec(0.0, 1.0), default(0.5))
-        val ry0   = pAudio("ry", ParamSpec(0.0, 1.0), default(0.5))
-        val rx    = rx0 * width
-        val ry    = ry0 * height
+        val rx    = pAudio("rx", ParamSpec(0.0, 1.0), default(0.5))
+        val ry    = pAudio("ry", ParamSpec(0.0, 1.0), default(0.5))
+//        val rx    = rx0 * width
+//        val ry    = ry0 * height
 
         val freq0 = pAudio("freq", ParamSpec(0.001, 10000.0, ExponentialWarp), default(0.5))
         val freq  = freq0 // .linexp(0, 1, 0.001, 10000)
@@ -111,10 +181,12 @@ object Cracks {
         val twopi = math.Pi * 2
         val phase = Phasor.ar(speed = twopi * 2 * freq / SampleRate.ir, lo = 0, hi = twopi)
 
-        val x = (cx + rx * phase.cos).fold(0, width )
-        val y = (cy + ry * phase.sin).fold(0, height)
+        val x   = ((cx + rx * phase.cos) * width ).fold(0, width )
+        val y   = ((cy + ry * phase.sin) * height).fold(0, height)
+        val xm  = Mix.mono(x) / NumChannels(x)
+        val ym  = Mix.mono(y) / NumChannels(y)
 
-        ReplaceOut.ar(busXY, Seq(Mix.mono(x) / width, Mix.mono(y) / height))
+        ReplaceOut.ar(busXY, Seq(xm / width, ym / height))
 
         val pos = y.roundTo(1) * width + x
 
